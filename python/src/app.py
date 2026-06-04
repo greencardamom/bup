9d7edf0f5d66863bedb979cfb38c1ad55f329cad
greencardamom <seucxzya0c8dmle4tulz@nym.hush.com>
2026-06-04 00:56:13 -0400

    Python 3.11 upgrade: pin deps to modern stack (Flask 3.x), fix Last-Modified header for Werkzeug 3

--- python/src/app.py
+++ python/src/app.py
@@ -15,7 +15,8 @@ import os
 import json
 import random
 import string
-from datetime import datetime, date
+from datetime import date
+from email.utils import formatdate
 from functools import wraps, update_wrapper
 
 import flask
@@ -77,7 +78,8 @@ def nocache(view):
     @wraps(view)
     def no_cache(*args, **kwargs):
         response = make_response(view(*args, **kwargs))
-        response.headers['Last-Modified'] = datetime.now()
+        # HTTP-date string (Werkzeug 3 wants str header values, not a datetime).
+        response.headers['Last-Modified'] = formatdate(usegmt=True)
         response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
         response.headers['Pragma'] = 'no-cache'
         response.headers['Expires'] = '-1'
